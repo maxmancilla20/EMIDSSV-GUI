8b3ba4f31b5ee31e32d859cb131d265cf26e1d27
Maximiliano Mancilla <mmancilla.torres@iteso.mx>
2024-07-16 00:08:54 -0600

    Functionality Complete. Missing to add uart configurtion in a new window.

--- EMIDSSV_GUI.py
+++ EMIDSSV_GUI.py
@@ -2,28 +2,22 @@ import serial
 import serial.tools.list_ports as list_ports
 import PySimpleGUI as sg
 from PIL import Image
+import time
 
-port_name = "COM22"  # the name / address we found for our device
+port_name = "COM20"  # the name / address we found for our device
 
 ser = serial.Serial(
     port=port_name,
     baudrate=9600,
     bytesize=serial.EIGHTBITS,  # set this to the amount of data you want to send
+    timeout=1
     )
 
-# the information we want to send: 8 bits = 1 byte
-#byte_to_send = b"Hola soy python"
-#ser.write(byte_to_send)
-
-# After measurements are done, close the connection
-#ser.close()
-# Function to be called when the first button is pressed
-def first_button_function():
-    print("First button pressed!")
-
-# Function to be called when the send button is pressed
-def send_command_function(command):
-    print(f"Command sent: {command}")
+# Verificar si el puerto está abierto
+if ser.is_open:
+    print(f'Conectado a {ser.portstr}')
+else:
+    print('Error al abrir el puerto')
 
 # Resize the image
 def resize_image(image_path, size, name):
@@ -50,82 +44,99 @@ exit_path = 'exit.png'
 exit_size = (25, 25)  # Adjust the size as needed
 exit_png = resize_image(exit_path, exit_size, "exit")
 
-combo_read  = sg.Combo(['Memory', 'Sensor', 'Hour'], font=('Arial Bold', 10), enable_events=True, size=(12,1),key='-COMBO READ-')
+"""====================================================== Layout configurations ============================================================="""
+combo_read  = sg.Combo(['Time', 'Memory', 'SW Version'], font=('Arial Bold', 10), enable_events=True, size=(12,1),key='-COMBO READ-')
 combo_write = sg.Combo(['Hour'], font=('Arial Bold', 10), enable_events=True,size=(12,1), key='-COMBO WRITE-')
-combo_reset = sg.Combo(['Memory', 'Sensor', 'EMIDSS'], font=('Arial Bold', 10), enable_events=True, size=(12,1), key='-COMBO RESET-')
-blank_space_1 = sg.Text('                         ',background_color='white', key=None)
-blank_space_2 = sg.Text('                         ',background_color='white', key=None)
-Read_Service_Text  = sg.Text('Read Service',  font=("Verdana Bold",12), text_color="black", size=(27, 1), background_color='white')
-Write_Service_Text = sg.Text('Write Service', font=("Verdana Bold",12), text_color="black", size=(28, 1), background_color='white')
-Reset_Service_Text = sg.Text('Reset Service', font=("Verdana Bold",12), text_color="black", size=(15, 1), background_color='white')
+combo_reset = sg.Combo(['Sensor', 'Memory'], font=('Arial Bold', 10), enable_events=True, size=(12,1), key='-COMBO RESET-')
+blank_space_1 = sg.Text('                   ',background_color='white', key=None)
+blank_space_2 = sg.Text('                   ',background_color='white', key=None)
+Read_Service_Text  = sg.Text('Read Service',  font=("Verdana Bold",12), text_color="black", size=(25, 1), background_color='white')
+Write_Service_Text = sg.Text('Write Service.   Bytes', font=("Verdana Bold",12), text_color="black", size=(30, 1), background_color='white')
+Reset_Service_Text = sg.Text(' Reset Service', font=("Verdana Bold",12), text_color="black", size=(15, 1), background_color='white')
 Raw_Message_Text = sg.Text('Raw Message', font=("Verdana Bold",12), text_color="black", size=(12, 1), background_color='white')
 exit_button = sg.Button(image_filename='exit_png.png', key='-EXIT-')
 port_button = sg.Button('Get Ports', key='-GET PORTS-')
+Write_Input_1 = sg.Input(size=(2,1), key='-WRITE IN 1-')
+Write_Input_2 = sg.Input(size=(2,1), key='-WRITE IN 2-')
 
 # Layout of the window
 layout = [
     [sg.Text('',background_color='white', size=(33,1)), sg.Image(filename=emidss_png, key='-IMAGE-'), sg.Text('', font=("Verdana", 20), text_color="blue", size=(15, 1), background_color='white')],
     [Read_Service_Text, Write_Service_Text, Reset_Service_Text],
-    [combo_read, sg.Button(image_filename='send_png.png', border_width=None, key='-SEND READ-'), blank_space_1,  combo_write, sg.Button(image_filename='send_png.png', border_width=None, key='-SEND WRITE-'), blank_space_2,  combo_reset, sg.Button(image_filename='send_png.png', border_width=None, key='-SEND RESET-')],
+    [combo_read, sg.Button(image_filename='send_png.png', border_width=None, key='-SEND READ-'), blank_space_1,  combo_write, Write_Input_1, Write_Input_2, sg.Button(image_filename='send_png.png', border_width=None, key='-SEND WRITE-'), blank_space_2,  combo_reset, sg.Button(image_filename='send_png.png', border_width=None, key='-SEND RESET-')],
     [sg.Text('________________________________________________________________________', font=("Verdana Bold",12), text_color="black", size=(72, 1), background_color='white')],
     [Raw_Message_Text, sg.Input(size=(20,1), key='-COMMAND-'), sg.Button(image_filename='send_png.png', border_width=None, key='-SEND RAW-'), sg.Text('',background_color='white', size=(27,1)), port_button],
-    [sg.Output(size=(90, 10))],  # Output area for displaying logs
+    [sg.Multiline(size=(80, 20), key='-OUTPUT-',text_color="green", autoscroll=True, reroute_stdout=True, reroute_stderr=True, echo_stdout_stderr=True) ],
     [exit_button]
 ]
 
+"""====================================================== APPLICATION ============================================================="""
+
 # Create the window
-window = sg.Window("EMIDSS-V GUI", layout, background_color='white')
+window = sg.Window("EMIDSS-V GUI", layout, background_color='white', finalize=True)
 
 # Event loop
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=100)
     
     if event == sg.WIN_CLOSED or event == '-EXIT-':
         break
-            
+    """============= READ SERVICES ==============="""        
     if event == '-SEND READ-':
         read_action = values['-COMBO READ-']
-        if read_action == 'Memory':
+        
+        if read_action == 'Time':
+            print('Reading Time')
+            
+            ser.write(b"S2201")
+            ser.write(b"S2201")
+            
+        elif read_action == 'Memory':
             print('Reading Memory')
             
-            byte_to_send = b"S2203"
-            ser.write(byte_to_send)
-            ser.write(byte_to_send)
-
-        elif read_action == 'Sensor':
-            print('Reading Sensor')
+            ser.write(b"S2202")
+            ser.write(b"S2202")
             
-            byte_to_send = b"S2201"
-            #ser.write(byte_to_send)
-            #ser.write(byte_to_send)
-
-        elif read_action == 'Hour':
-            print('Reading Hour')
+        elif read_action == 'SW Version':
+            print('Reading SW Version')
             
-            byte_to_send = b"S2202"
-            #ser.write(byte_to_send)
-            #ser.write(byte_to_send)
-    
+            ser.write(b"S2203")
+            ser.write(b"S2203")
+
+    """============= WRITE SERVICES ==============="""  
     if event == '-SEND WRITE-':
         read_action = values['-COMBO WRITE-']
+        read_input_1 = values['-WRITE IN 1-']
+        read_input_2 = values['-WRITE IN 2-']
+        
         if read_action == 'Hour':
-            print('Performing WRITE MEM...')
+            print('Write Time [' + read_input_1 + ':' + read_input_2 + ']')
             
+            ser.write(("S2301" + read_input_1 + read_input_2).encode('utf-8'))
+            ser.write(("S2301" + read_input_1 + read_input_2).encode('utf-8'))
+    
+    """============= RESET SERVICES ==============="""          
     if event == '-SEND RESET-':
         read_action = values['-COMBO RESET-']
-        if read_action == 'Memory':
-            print('Performing reset MEM...')
-        elif read_action == 'Sensor':
-            print('Performing Sensor reset...')
-
-        elif read_action == 'EMIDSS':
-            print('Performing emidss reset...')
+        
+        if read_action == 'Sensor':
+            print('Reset Sensor...')
+            ser.write(b"S1101")
+            ser.write(b"S1101")
             
-               
+        elif read_action == 'Memory':
+            print('Reset Memory...')
+            ser.write(b"S1102")
+            ser.write(b"S1102")
+            
+    """============= RAW SERVICES ==============="""             
     if event == '-SEND RAW-':
         command = values['-COMMAND-']
-        send_command_function(command)
-        
+        print('Raw Command Sent: ' + command)
+        ser.write((command).encode('utf-8'))
+        ser.write((command).encode('utf-8'))
+     
+    """============= GET PORTS BUTTON ==============="""   
     if event == '-GET PORTS-':
         # List all comports
         all_ports = list_ports.comports()
@@ -136,7 +147,16 @@ while True:
         first_serial_device = all_ports[0]
         print(first_serial_device.device)  # the `port_name`
         print(first_serial_device.description)  # perhaps helpful to know if this is your device
-
-     
+    
+    """============= READ UART FUNCTION ==============="""    
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        #print(f'{line}')
+        window['-OUTPUT-'].print(line, text_color='blue')
+        time.sleep(0.01)# Pausa para evitar una sobrecarga de la CPU
+    
+    
+"""================================================================================================================================"""
+          
 window.close()
 ser.close()
